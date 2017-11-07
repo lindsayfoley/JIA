ef4be0cd5e34ffbad3c92738ff26970f8335ecda
Lindsay Foley <lindsay.foley@outlook.com>
2017-11-07 20:37:19 +0000

    - Cleaning up the repo

---
+++ public/blog.php
@@ -0,0 +1,73 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<?php include('structure/meta.php'); ?>
+<title>Jesus In Action Blog | Jesus In Action Charity</title>
+<meta name="description" content="Catch up with the latest news on our blog. Jesus in Action aims to provide safe homes for vulnerable, excluded, and deprived children to help them achieve their full potential. Read our blog for the latest updates on this charity.">
+<meta name="keywords" content="jesus in action, childrens charity, childrens charities, orphan charity, orphan charities, donate to childrens charity, donate to childrens charities, guyana orphans, orphans in guyana, children in guyana, guyana charity, guyana charities, south american charity, south american charities">
+<link rel="stylesheet" type="text/css" href="style/blog.css">
+<?php include('structure/styles.php'); ?>
+<?php include('structure/icons.php'); ?>
+<?php include('structure/scripts.php'); ?>
+<?php include('structure/legacy.php'); ?>
+
+</head>
+
+<body class="offcanvas-closed">
+
+<div id="container">
+
+	<!-- Mobile hamburger -->
+	<header>
+		<div id="hamburger">
+			<div>&nbsp;</div>
+			<div>&nbsp;</div>
+			<div>&nbsp;</div>
+		</div>
+		<a href="\"><img id="mobile_logo" src="images/logo.png" alt="Jesus In Action logo"></a>
+	</header>
+	<!-- /Mobile hamburger -->
+	
+	<!-- Mobile & Desktop Nav -->
+	<div id="header">
+		<?php include('structure/nav.php'); ?>
+	</div>
+	<!-- /Mobile & Desktop Nav -->
+
+	<main>
+		<div>
+			<h2>Home interiors</h2>
+			<h3>25th January 2015</h3>
+				<p>The plumbing for the bathrooms is virtually complete! The bedrooms have been fitted with decorative security grills, and are having fitted wardrobes constructed 
+				in each room.</p>
+		</div>
+		<div>
+			<h2>Visitors from the UK make a visit to Guyana</h2>
+			<h3>25th January 2015</h3>
+				<p>Heather Rostron, Camille Ali and Lisa Joseph made a trip in March and November 2014 to help with the project. Camille and Lisa helped to design the fitted wardrobes
+				for the bedrooms. Heather created a photo diary and is currenly working on some short videos - hopefully we can showcase these here soon.</p>
+		</div>
+		<div>
+			<h2>Helping the community with supplies</h2>
+			<h3>25th January 2015</h3>
+				<p>We gave out shoes, clothes, food and supplies donated by various supporters in the UK to people in Parika. We want to especially thank Beacon Of Hope Ministry in
+				Shirley for donating 2 barrels, these were gratefully received.</p>
+		</div>
+		<div>
+			<h2>Giving the visitors a view of Guyanas unspoilt interior</h2>
+			<h3>25th January 2015</h3>
+				<p>We visited Kaiteur falls in Novemeber 2014, which is an hours flight from the capital city Georgetown. Kaiteur is the third highest single drop waterfall in 
+				the world.</p>
+		</div>
+		<div>
+			<h2>We have a great new mobile site</h2>
+			<h3>8th August 2014</h3>
+				<p>Have a look at our mobile site - we have just finished it and are really happy with our super duper new mobile optimised version!</p>
+		</div> 
+		<?php include('structure/footer.php'); ?>
+	</main>
+ 
+</div> 
+
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,52 @@
+﻿<!DOCTYPE html>
+<html><head>
+<title>Home | Jesus In Action Charity</title>
+<?php include('structure/meta.php'); ?>
+<meta name="description" content="Help us build a better future for children in Guyana. Jesus in Action aims to provide safe homes for vulnerable, excluded, and deprived children to help them achieve their full potential. Our first home will be opening up soon, October 2016.">
+<meta name="keywords" content="jesus in action, childrens charity, childrens charities, orphan charity, orphan charities, donate to childrens charity, donate to childrens charities, guyana orphans, orphans in guyana, children in guyana, guyana charity, guyana charities, south american charity, south american charities">
+<?php include('structure/styles.php'); ?>
+<?php include('structure/icons.php'); ?>
+<?php include('structure/scripts.php'); ?>
+<?php include('structure/legacy.php'); ?>
+
+</head>
+
+<body class="offcanvas-closed">
+
+<div id="container">
+
+	<!-- Mobile hamburger -->
+	<header>
+		<div id="hamburger">
+			<div>&nbsp;</div>
+			<div>&nbsp;</div>
+			<div>&nbsp;</div>
+		</div>
+		<a href="\"><img id="mobile_logo" src="images/logo.png" alt="Jesus In Action logo"></a>
+	</header>
+	<!-- /Mobile hamburger -->
+	
+	<!-- Mobile & Desktop Nav -->
+	<div id="header">
+		<?php include('structure/nav.php'); ?>
+	</div>
+	<!-- /Mobile & Desktop Nav -->
+
+	<main>		
+		<div id="homepage">
+			<a id="help_us_image" href="support.php" title="help us with our mission">
+				<img src="images/hp_hero.jpg" alt="help us with our mission">
+			</a>
+			<div id="welcome_text" class="stack">
+				<h2>Welcome to the Jesus in Action website</h2>
+				<p>Every child has the right to a home and education, and our aim is to make a positive and lasting change in the lives of the children we work with.</p>
+				<p>By providing a safe and secure home, and by teaching them skills in a rural environment away from the city, we believe that we can help some of the most deprived children in Guyana.</p> 
+				<p>Help us build a better future for children in Guyana</p>
+			</div> 
+ 		</div>
+ 		<?php include('structure/footer.php'); ?>
+	</main>
+</div> 
+
+</body>
+</html>
