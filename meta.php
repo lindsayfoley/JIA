9805f99feb276e1c880f39acfa673a662f015301
Lindsay Foley <lindsay.foley@outlook.com>
2017-11-07 20:31:25 +0000

    - Cleaning up the repo

--- meta.php
+++
@@ -1,4 +0,0 @@
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta charset="UTF-8">
-<meta content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width" name="viewport">
-<meta name="author" content="Lindsay Foley">
--- styles.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" type="text/css" href="/style/jia.css">
-<link rel="stylesheet" type="text/css" href="/style/jia_mobile.css">
