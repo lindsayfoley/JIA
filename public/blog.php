e8a902995abd866427e3646db6679d4fef7fd6fa
Lindsay Foley <lindsay.foley@outlook.com>
2017-11-09 20:30:27 +0000

    - All pages now have Foundation

--- public/blog.php
+++ public/blog.php
@@ -5,7 +5,6 @@
 <title>Jesus In Action Blog | Jesus In Action Charity</title>
 <meta name="description" content="Catch up with the latest news on our blog. Jesus in Action aims to provide safe homes for vulnerable, excluded, and deprived children to help them achieve their full potential. Read our blog for the latest updates on this charity.">
 <meta name="keywords" content="jesus in action, childrens charity, childrens charities, orphan charity, orphan charities, donate to childrens charity, donate to childrens charities, guyana orphans, orphans in guyana, children in guyana, guyana charity, guyana charities, south american charity, south american charities">
-<link rel="stylesheet" type="text/css" href="style/blog.css">
 <?php include('structure/styles.php'); ?>
 <?php include('structure/icons.php'); ?>
 <?php include('structure/scripts.php'); ?>
@@ -21,33 +20,33 @@
 	</div>
 
 	<main>
-		<div class="large-12 row">
+		<div class="large-12 column">
 			<h2>Home interiors</h2>
-			<h3>25th January 2015</h3>
+			<h3 class="sub_header">25th January 2015</h3>
 				<p>The plumbing for the bathrooms is virtually complete! The bedrooms have been fitted with decorative security grills, and are having fitted wardrobes constructed 
 				in each room.</p>
 		</div>
-		<div class="large-12 row">
+		<div class="large-12 column">
 			<h2>Visitors from the UK make a visit to Guyana</h2>
-			<h3>25th January 2015</h3>
+			<h3 class="sub_header">25th January 2015</h3>
 				<p>Heather Rostron, Camille Ali and Lisa Joseph made a trip in March and November 2014 to help with the project. Camille and Lisa helped to design the fitted wardrobes
 				for the bedrooms. Heather created a photo diary and is currenly working on some short videos - hopefully we can showcase these here soon.</p>
 		</div>
-		<div class="large-12 row">
+		<div class="large-12 column">
 			<h2>Helping the community with supplies</h2>
-			<h3>25th January 2015</h3>
+			<h3 class="sub_header">25th January 2015</h3>
 				<p>We gave out shoes, clothes, food and supplies donated by various supporters in the UK to people in Parika. We want to especially thank Beacon Of Hope Ministry in
 				Shirley for donating 2 barrels, these were gratefully received.</p>
 		</div>
-		<div class="large-12 row">
+		<div class="large-12 column">
 			<h2>Giving the visitors a view of Guyanas unspoilt interior</h2>
-			<h3>25th January 2015</h3>
+			<h3 class="sub_header">25th January 2015</h3>
 				<p>We visited Kaiteur falls in Novemeber 2014, which is an hours flight from the capital city Georgetown. Kaiteur is the third highest single drop waterfall in 
 				the world.</p>
 		</div>
-		<div class="large-12 row">
+		<div class="large-12 column">
 			<h2>We have a great new mobile site</h2>
-			<h3>8th August 2014</h3>
+			<h3 class="sub_header">8th August 2014</h3>
 				<p>Have a look at our mobile site - we have just finished it and are really happy with our super duper new mobile optimised version!</p>
 		</div> 
 		<?php include('structure/footer.php'); ?>
